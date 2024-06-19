fbed8f85b2611e0381d74b0b01e770368aa235c0
Anfelesan <anfelesan@gmail.com>
2024-06-18 23:28:31 -0500

    [Advance] Advance in front

--- App/Front-end/src/components/catedras/CatedraCard/CatedraCard.jsx
+++ App/Front-end/src/components/catedras/CatedraCard/CatedraCard.jsx
@@ -1,10 +1,24 @@
-const CatedraCard = ({  }) => {
+import css from './CatedraCard.module.css'
+
+const CatedraCard = ({ name, initDate, endDate, id }) => {
+  const urlSeeMore = 'ver-catedra-especifica/' + id
+
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className={css.catedraCard}>
+      <div className={css.divName}>
+        <span className={css.spanName}>{name}</span>
+      </div>
+      <div className={css.divInitDate}>
+        <span className={css.spanTitleInitDate}>Fecha de inicio</span>
+        <span className={css.spanInitDate}>{initDate}</span>
+      </div>
+      <div className={css.divEndDate}>
+        <span className={css.spanTitleEndDate}>Fecha de finalización</span>
+        <span className={css.spanEndDate}>{endDate}</span>
+      </div>
+      <div className={css.divButton}>
+        <a href={urlSeeMore} className={css.button}>Ver más</a>
+      </div>
     </div>
   )
 }
